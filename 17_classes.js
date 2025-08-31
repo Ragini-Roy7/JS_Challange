3c44d9dfced203e407009a0c5d63d7c3a3b7e80d
RAGINI ROY <120214163+Ragini-Roy7@users.noreply.github.com>
2025-08-31 21:42:56 +0530

    Add files via upload

--- 17_classes.js
+++ 17_classes.js
@@ -13,11 +13,74 @@ class Employee{
 }
 const emp= new Employee("Shyam Khatri",19);
 const emp2= new Employee("Puja Sharma",22)
-console.log(emp.getDetails());
-console.log(emp.greet());
-console.log(emp2.getDetails());
-console.log(emp2.greet());
+// console.log(emp.getDetails());
+// console.log(emp.greet());
+// console.log(emp2.getDetails());
+// console.log(emp2.greet());
+
+//prob 3
+class Car{
+    //default color set
+    constructor(brand,color="black"){
+        this.brand=brand;
+        this.color= color;
+    }
+    //set methods
+    getProperties(){
+        return `Car ${this.brand} is having ${this.color} color`
+    }
+}
+//create instances
+const car= new Car("Sizuki", "Red")
+const car2= new Car("Mahindra")
+// console.log(car.getProperties());
+// console.log(car2.getProperties());
 
 
+//prob 4
+const readline= require("readline");
+class BankAccount{
+    constructor(owner,balance=0.0){
+        this.owner=owner;
+        this.balance=balance;
+    }
+    //set methods
+deposit(amount) {
+    if (amount>0){
+        this.balance+= amount;
+        console.log(`Deposited amount is ${amount}. New Account balance is ${this.balance}`)
+    }
+}
+withdraw(amount){
+    if(amount<= this.balance){
+        this.balance-= amount;
+        console.log( `Withdawl amount is ${amount}. Updated balance is ${this.balance}`)
+    }
+    else {
+        console.log(`Insufficient balance.`)
+    }
+}
+//show
+showTransactions(){
 
- 
+}
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question("Enter owner name :", (name)=> {
+    //create instances
+    const account= new BankAccount(name);
+    rl.question("Enter amount to deposit:", (input)=> {
+        const amount= parseFloat(input);
+        account.deposit(amount);
+
+    rl.question("Enter amount for withdrawl:", (input)=> {
+        const amount= parseFloat(input);
+        account.withdraw(amount);
+        rl.close();
+    })
+    })
+});
